fe3ac978000aa21e05cdb539aa641b839873566c
andreimladin <andrei.mladin@gmail.com>
2014-03-04 13:18:09 +0200

    Revert "AIM - Updated a drop query (added the "if" check before removing)"
    This reverts commit 20bb4f626b852eb006aa49a9e9d760bf12c6b32f.

--- src/main/db/tables/tables_all.sql
+++ src/main/db/tables/tables_all.sql
@@ -277,7 +277,7 @@ CREATE TABLE AccountConfirmation (
 
 ALTER TABLE AccountConfirmation ADD FOREIGN KEY (UserId) REFERENCES Users(UserId);
 
-DROP TABLE IF EXISTS UserDownloads;
+DROP TABLE UserDownloads;
 
 CREATE TABLE UserDownloads (
   UserId INTEGER NOT NULL,
